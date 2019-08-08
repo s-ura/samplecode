9758aa5e3d7f08f4afcd35d6916da71a2f2d3941
aShinjiroUrata <shinjiro.urata@access-company.com>
2019-08-08 16:30:49 +0900

    WIP-3: multi-level-popup ver1 done

--- popup-test/src/example.js
+++ popup-test/src/example.js
@@ -1,6 +1,8 @@
-import React from 'react'
+import React from 'react';
 import _ from 'lodash';
-import { Popup, Grid } from 'semantic-ui-react'
+import { Button, Popup, Grid } from 'semantic-ui-react';
+
+import Styles from './example.css';
 
 const general_setting_items = [
   //'Accounts',
@@ -15,12 +17,14 @@ const service_items = [
   'Connected Car',
 ];
 
+
 const SubPopup = (props) => {
   const gridItems = _.map(props.items, (item) => {
     return (
       <Grid.Row>
         <Grid.Column
-          textAlign='left'>
+          className={Styles.hoverContentItem}
+          >
           <b>{item}</b>
         </Grid.Column>
       </Grid.Row>
@@ -29,24 +33,33 @@ const SubPopup = (props) => {
 
   return (
   <Popup
+    className={Styles.popup}
     trigger={<b>{props.name}</b>}
-    hoverable
+    //hoverable
     position='right center'
+    on='click'
+    pinned
     >
-    <Grid rows='equal' divided='vertically'>
+    <Grid
+      textAlign='left'
+      className={Styles.hoverContent}
+      rows='equal' divided='vertically'>
       {gridItems}
     </Grid>
   </Popup>
   );
 }
 
+
 const PopupExampleFlowing = () => {
 
   const gridItems = _.map(general_setting_items, (item) => {
     return (
-      <Grid.Row>
+      <Grid.Row
+      >
       <Grid.Column
-        textAlign='left'>
+        className={Styles.hoverContentItem}
+        >
         <SubPopup
           name={item}
           items={service_items}
@@ -58,11 +71,17 @@ const PopupExampleFlowing = () => {
 
   return (
     <Popup
+      className={Styles.popup}
       trigger={<b>General Setting</b>}
-      hoverable
+      //hoverable
       position='right center'
+      on='click'
+      pinned
       >
-      <Grid rows='true' divided='vertically'>
+      <Grid
+      textAlign='left'
+      className={Styles.hoverContent}
+      rows='equal' divided='vertically'>
         {gridItems}
       </Grid>
     </Popup>
